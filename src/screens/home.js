4b9e4cb7badf75bf7b8e2471ee43b495a2f5e318
Ro7Rinke <ro7rinke@gmail.com>
2020-06-14 21:03:14 -0300

    de

--- src/screens/home.js
+++ src/screens/home.js
@@ -1,46 +1,20 @@
-import React from 'react'
+import React from 'react';
 import {
     View,
     Text,
-    StyleSheet,
-    Image
-} from 'react-native'
-import Icon from 'react-native-vector-icons/MaterialIcons'
-
-import globalStyles from "../styles/globalStyles"
-import colors from "../styles/colors"
-
-
-const saude = require('../../assets/images/saude.png')
+    StyleSheet
+} from 'react-native';
 
 export default Home = () => {
     return (
-        <View style={globalStyles.globalContainer}>
-            <View style={{ width: "100%", justifyContent: "center", marginTop: "5%", marginLeft: "3%" }}>
-                <Text style={[globalStyles.title, {}]}>PodCast ao Vivo:</Text>
-            </View>
-            <View style={{ height: "30%", marginTop: "5%", alignItems: "center", justifyContent: "space-between", flexDirection: "row" }}>
-                <View style={{ margin: 10, height: 125, width: 125, backgroundColor: colors.gray }}>
-                    <Image source={saude} resizeMode={"cover"} style={{ height: "100%", width: "100%" }} />
-
-                </View>
-                <View style={{ width: "40%" }}>
-                    <Text style={{ fontSize: 20, fontWeight: "bold" }}>PodCast Saúde</Text>
-                    <Text>Dr. José Antônio explica os beneficios da boa alimentação</Text>
-                    <Text style={{ fontSize: 17, fontWeight: "bold", color: "#e61919" }}>● AO VIVO</Text>
-                </View>
-                <Icon name="play-circle-outline" size={70} style={{ marginRight: "5%" }} />
-            </View>
-            <View style={globalStyles.hr} />
-            <View style={{ width: "100%", justifyContent: "center", marginTop: "5%", marginLeft: "3%" }}>
-                <Text style={[globalStyles.title, {}]}>PodCast ao Vivo:</Text>
-            </View>
+        <View>
+            <Text>Um texto aqui só pra falar q fununfou</Text>
         </View>
     )
 }
 
 const styles = StyleSheet.create({
-    title: {
+    container: {
 
     },
 
