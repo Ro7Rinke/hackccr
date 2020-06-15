c48ba3a024e2470812475cb55964e30b18cf1a1e
Ro7Rinke <ro7rinke@gmail.com>
2020-06-14 22:41:46 -0300

    imgs

--- src/screens/podcast.js
+++ src/screens/podcast.js
@@ -12,12 +12,17 @@ import {
 import { Actions } from 'react-native-router-flux'
 
 const img1 = require('../../assets/images/saude.png')
+const img2 = require('../../assets/images/truckMotor.jpg')
+const img3 = require('../../assets/images/women.jpg')
+const img4 = require('../../assets/images/beach-4316959_1920.jpg')
+
+
 export default Podcast = () => {
     let data = [
         { id: '1', image: img1, title: 'Saude' },
-        { id: '12', image: img1, title: 'Segurança' },
-        { id: '13', image: img1, title: 'CCR' },
-        { id: '14', image: img1, title: 'Notícias' },
+        { id: '12', image: img2, title: 'Segurança' },
+        { id: '13', image: img3, title: 'CCR' },
+        { id: '14', image: img4, title: 'Notícias' },
     ]
 
     const renderItem = ({ item }) => {
